bb9d35b218e64de2fe71f48875e63760b5e4f0de
Jihyuk Lee <lead@sver.dev>
2025-04-17 16:17:05 +0900

    style :: ktlint 설치 및 구동

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,24 +1,20 @@
 plugins {
     kotlin("jvm") version "1.9.25"
-//    kotlin("plugin.spring") version "1.9.25"
-//    id("org.springframework.boot") version "3.4.4"
-//    id("io.spring.dependency-management") version "1.1.7"
-//    kotlin("plugin.jpa") version "1.9.25"
+    id("org.jlleitschuh.gradle.ktlint").version("12.2.0")
 }
-
 
 subprojects {
     apply(plugin = "org.jetbrains.kotlin.jvm")
 
     dependencies {
-        /* kotlin */
+        // kotlin
         implementation("org.jetbrains.kotlin:kotlin-reflect")
 
-        /* lombok */
+        // lombok
         compileOnly("org.projectlombok:lombok")
         annotationProcessor("org.projectlombok:lombok")
 
-        /* junit */
+        // junit
         testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
         testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     }
@@ -27,7 +23,6 @@ subprojects {
 allprojects {
     group = "org.aing"
     version = "0.0.1-SNAPSHOT"
-
 
     java {
         toolchain {
