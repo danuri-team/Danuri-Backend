c5165a9be715aaf2b6f7990461f3047d28db6e6d
Jihyuk Lee <lead@sver.dev>
2025-04-22 10:55:15 +0900

    feat :: DTO 상세 제약 조건 추가

--- danuri-rest/src/main/kotlin/org/aing/danurirest/domain/space/dto/IsUsingSpaceResponse.kt
+++ danuri-rest/src/main/kotlin/org/aing/danurirest/domain/space/dto/IsUsingSpaceResponse.kt
@@ -1,5 +1,8 @@
 package org.aing.danurirest.domain.space.dto
 
+import jakarta.validation.constraints.NotNull
+
 data class IsUsingSpaceResponse(
+    @field:NotNull(message = "공간 사용 여부는 필수입니다.")
     val isUsingSpace: Boolean,
 )
--- danuri-rest/src/main/kotlin/org/aing/danurirest/domain/space/dto/RentalSimpleResponse.kt
+++ danuri-rest/src/main/kotlin/org/aing/danurirest/domain/space/dto/RentalSimpleResponse.kt
@@ -1,15 +1,22 @@
 package org.aing.danurirest.domain.space.dto
 
+import jakarta.validation.constraints.NotNull
+import jakarta.validation.constraints.PastOrPresent
 import org.aing.danuridomain.persistence.rental.entity.Rental
 import java.time.LocalDateTime
 import java.util.UUID
 
 data class RentalSimpleResponse(
     val id: UUID?,
+    @field:NotNull(message = "아이템 ID는 필수입니다.")
     val itemId: UUID?,
+    @field:NotNull(message = "아이템 이름은 필수입니다.")
     val name: String,
+    @field:NotNull(message = "대여 시작 시간은 필수입니다.")
+    @field:PastOrPresent(message = "대여 시작 시간은 현재 또는 과거여야 합니다.")
     val borrowedAt: LocalDateTime,
     val returnedAt: LocalDateTime?,
+    @field:NotNull(message = "반납 여부는 필수입니다.")
     val isReturned: Boolean,
 ) {
     companion object {
