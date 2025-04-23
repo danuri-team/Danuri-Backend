22ca5b9e7d73c20a01776040cfdc8954350bfeda
Jihyuk Lee <lead@sver.dev>
2025-04-23 10:09:47 +0900

    lint :: 간단한 줄 변경

--- danuri-rest/src/main/kotlin/org/aing/danurirest/domain/item/dto/ItemRentalRequest.kt
+++ danuri-rest/src/main/kotlin/org/aing/danurirest/domain/item/dto/ItemRentalRequest.kt
@@ -7,8 +7,7 @@ import java.util.UUID
 data class ItemRentalRequest(
     @field:NotNull(message = "아이템 ID는 필수입니다.")
     val itemId: UUID,
-    
     @field:NotNull(message = "대여 수량은 필수입니다.")
     @field:Positive(message = "대여 수량은 1 이상이어야 합니다.")
-    val quantity: Int
+    val quantity: Int,
 ) 
