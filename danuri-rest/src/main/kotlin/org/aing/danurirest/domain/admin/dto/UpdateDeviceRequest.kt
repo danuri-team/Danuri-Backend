5f9ecfa603b0525dc1179ab38eca223681e1dc24
Jihyuk Lee <lead@sver.dev>
2025-05-05 23:10:51 +0900

    feat :: DTO 제약사항 추가

--- danuri-rest/src/main/kotlin/org/aing/danurirest/domain/admin/dto/UpdateDeviceRequest.kt
+++ danuri-rest/src/main/kotlin/org/aing/danurirest/domain/admin/dto/UpdateDeviceRequest.kt
@@ -6,6 +6,6 @@ import java.util.UUID
 data class UpdateDeviceRequest(
     @field:NotNull(message = "공간 ID는 필수 입력값입니다.")
     val spaceId: UUID,
-    
+    @field:NotNull(message = "활성화 여부는 필수 입력값입니다.")
     val isActive: Boolean
 ) 
