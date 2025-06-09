fc435bcc40258d8dbc76911e694e6eccb8f83036
Jihyuk Lee <lead@sver.dev>
2025-06-09 20:52:06 +0900

    feat :: 실제 문자 API 연동

--- danuri-rest/build.gradle.kts
+++ danuri-rest/build.gradle.kts
@@ -11,15 +11,23 @@ dependencies {
     implementation("org.springframework.security:spring-security-crypto")
     implementation("org.springframework.boot:spring-boot-starter")
     implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework:spring-tx")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("io.jsonwebtoken:jjwt-api:0.12.6")
     runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.6")
     implementation("com.bucket4j:bucket4j-core:8.10.1")
+    implementation("net.nurigo:sdk:4.2.7")
     implementation("org.apache.poi:poi:5.4.1")
     implementation("org.apache.poi:poi-ooxml:5.4.1")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     runtimeOnly("io.micrometer:micrometer-registry-prometheus")
+}
+
+allOpen {
+    annotation("jakarta.persistence.Entity")
+    annotation("jakarta.persistence.MappedSuperclass")
+    annotation("jakarta.persistence.Embeddable")
 }
 
 tasks.getByName("jar") {
--- danuri-rest/src/main/kotlin/org/aing/danurirest/domain/auth/user/usecase/SendUserAuthCodeUsecase.kt
+++ danuri-rest/src/main/kotlin/org/aing/danurirest/domain/auth/user/usecase/SendUserAuthCodeUsecase.kt
@@ -1,15 +1,20 @@
 package org.aing.danurirest.domain.auth.user.usecase
 
+import net.nurigo.sdk.NurigoApp
+import net.nurigo.sdk.message.exception.NurigoMessageNotReceivedException
+import net.nurigo.sdk.message.model.Message
 import org.aing.danuridomain.persistence.user.entity.UserAuthCode
 import org.aing.danuridomain.persistence.user.repository.UserAuthCodeRepository
 import org.aing.danuridomain.persistence.user.repository.UserRepository
 import org.aing.danurirest.global.exception.CustomException
 import org.aing.danurirest.global.exception.enums.CustomErrorCode
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 import java.time.LocalDateTime
 import java.util.Random
 
 @Service
+@Transactional(rollbackFor = [Exception::class])
 class SendUserAuthCodeUsecase(
     private val userRepository: UserRepository,
     private val userAuthCodeRepository: UserAuthCodeRepository,
@@ -18,7 +23,7 @@ class SendUserAuthCodeUsecase(
         private const val AUTH_CODE_EXPIRE_MINUTES = 5L
     }
 
-    fun execute(phone: String): String {
+    fun execute(phone: String) {
         if (!userRepository.existsByPhone(phone)) {
             throw CustomException(CustomErrorCode.NOT_FOUND_USER)
         }
@@ -35,9 +40,13 @@ class SendUserAuthCodeUsecase(
                 expiredAt = expiredAt,
             )
         userAuthCodeRepository.save(userAuthCode)
-
-        // TODO: 뿌리오 연결
-        return authCode
+        val message = Message(from = "", to = phone, text = "[송정다누리센터] 인증번호는 $authCode 입니다. 본인이 아닐 경우, 문의 해주세요.")
+        try {
+            val messageService = NurigoApp.initialize(apiKey = "", apiSecretKey = "", domain = "https://api.solapi.com")
+            messageService.send(message)
+        } catch (e: NurigoMessageNotReceivedException) {
+            throw CustomException(CustomErrorCode.UNKNOWN_SERVER_ERROR)
+        }
     }
 
     private fun generateRandomCode(): String {
