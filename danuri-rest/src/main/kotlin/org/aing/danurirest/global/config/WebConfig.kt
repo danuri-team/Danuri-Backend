993ec4a729d2d40535d80ab3a5e7f4e7b2dab952
Jihyuk Lee <lead@sver.dev>
2025-06-12 17:12:53 +0900

    hotfix :: CORS 수정

---
+++ danuri-rest/src/main/kotlin/org/aing/danurirest/global/config/WebConfig.kt
@@ -0,0 +1,16 @@
+package org.aing.danurirest.global.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class WebConfig : WebMvcConfigurer {
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry
+            .addMapping("/**")
+            .allowedOrigins("http://localhost:3000")
+            .allowedMethods("*")
+            .allowCredentials(true)
+    }
+}
