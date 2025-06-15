8ccec8bdf01c80682656ce083b6da23fd8b650cc
Jihyuk Lee <lead@sver.dev>
2025-06-15 14:26:12 +0900

    hotfix :: JPA AUDIT 추가

--- danuri-domain/src/main/kotlin/org/aing/danuridomain/persistence/config/JpaConfiguration.kt
+++ danuri-domain/src/main/kotlin/org/aing/danuridomain/persistence/config/JpaConfiguration.kt
@@ -2,9 +2,11 @@ package org.aing.danuridomain.persistence.config
 
 import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.context.annotation.Configuration
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
 @Configuration
+@EnableJpaAuditing
 @EnableJpaRepositories(basePackages = ["org.aing.danuridomain.persistence"])
 @EntityScan(basePackages = ["org.aing.danuridomain.persistence"])
 class JpaConfiguration
