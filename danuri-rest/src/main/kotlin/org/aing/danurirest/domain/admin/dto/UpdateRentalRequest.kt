8acb7c6446206c07a0de38106033ff80ca967622
Jihyuk Lee <lead@sver.dev>
2025-06-16 21:12:40 +0900

    feat :: 0개 이상 값 받기

--- danuri-rest/src/main/kotlin/org/aing/danurirest/domain/admin/dto/UpdateRentalRequest.kt
+++ danuri-rest/src/main/kotlin/org/aing/danurirest/domain/admin/dto/UpdateRentalRequest.kt
@@ -7,7 +7,7 @@ import org.aing.danuridomain.persistence.rental.RentalStatus
 data class UpdateRentalRequest(
     @field:Min(value = 1, message = "총 수량은 최소 1개 이상이어야 합니다.")
     val quantity: Int,
-    @field:Min(value = 1, message = "반납 수량은 최소 1개 이상이어야 합니다.")
+    @field:Min(value = 0, message = "반납 수량은 0개 이상이어야 합니다.")
     val returnedQuantity: Int,
     @field:NotNull(message = "상태는 필수 값입니다.")
     val status: RentalStatus,
