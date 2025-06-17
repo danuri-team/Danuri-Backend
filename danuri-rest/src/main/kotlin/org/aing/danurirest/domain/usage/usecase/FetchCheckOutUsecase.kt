29a4eccddcd7c065bf9fc627ba805e68301c211a
Jihyuk Lee <lead@sver.dev>
2025-06-17 10:29:07 +0900

    chore :: 불필요한 임포트 제거

--- danuri-rest/src/main/kotlin/org/aing/danurirest/domain/usage/usecase/FetchCheckOutUsecase.kt
+++ danuri-rest/src/main/kotlin/org/aing/danurirest/domain/usage/usecase/FetchCheckOutUsecase.kt
@@ -1,7 +1,6 @@
 package org.aing.danurirest.domain.usage.usecase
 
 import org.aing.danuridomain.persistence.usage.repository.UsageHistoryRepository
-import org.aing.danuridomain.persistence.user.repository.UserRepository
 import org.aing.danurirest.global.security.jwt.dto.ContextDto
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.stereotype.Service
@@ -10,7 +9,6 @@ import java.time.LocalDateTime
 @Service
 class FetchCheckOutUsecase(
     val usageHistoryRepository: UsageHistoryRepository,
-    val userRepository: UserRepository,
 ) {
     fun execute() {
         val context = SecurityContextHolder.getContext().authentication.principal as ContextDto
