e41c444a8b90b4cd5baade7701e52c0cec308e97
Jihyuk Lee <lead@sver.dev>
2025-06-24 08:43:44 +0900

    hotfix :: 공간 제거시 사용 내역 고아 객체 제거

--- danuri-domain/src/main/kotlin/org/aing/danuridomain/persistence/space/entity/Space.kt
+++ danuri-domain/src/main/kotlin/org/aing/danuridomain/persistence/space/entity/Space.kt
@@ -1,13 +1,6 @@
 package org.aing.danuridomain.persistence.space.entity
 
-import jakarta.persistence.Column
-import jakarta.persistence.Entity
-import jakarta.persistence.FetchType
-import jakarta.persistence.GeneratedValue
-import jakarta.persistence.Id
-import jakarta.persistence.JoinColumn
-import jakarta.persistence.ManyToOne
-import jakarta.persistence.OneToMany
+import jakarta.persistence.*
 import org.aing.danuridomain.persistence.BaseEntity
 import org.aing.danuridomain.persistence.company.entity.Company
 import org.aing.danuridomain.persistence.usage.entity.UsageHistory
@@ -19,7 +12,7 @@ class Space(
     @Id
     @GeneratedValue
     val id: UUID? = null,
-    @OneToMany(mappedBy = "space", fetch = FetchType.LAZY)
+    @OneToMany(mappedBy = "space", fetch = FetchType.LAZY, cascade = [CascadeType.ALL], orphanRemoval = true)
     val usage: List<UsageHistory> = emptyList(),
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "company_id")
