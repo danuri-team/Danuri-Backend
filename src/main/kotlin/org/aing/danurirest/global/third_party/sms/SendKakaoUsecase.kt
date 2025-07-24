575376d5df1e11ec20e1c3f0475362fa90413970
Jihyuk Lee <withJihyuk@users.noreply.github.com>
2025-07-24 17:08:51 +0900

    feat :: 알림톡 발송 서비스 구현

--- src/main/kotlin/org/aing/danurirest/global/third_party/sms/SendKakaoUsecase.kt
+++ src/main/kotlin/org/aing/danurirest/global/third_party/sms/SendKakaoUsecase.kt
@@ -1,6 +1,7 @@
 package org.aing.danurirest.global.third_party.sms
 
 import net.nurigo.sdk.NurigoApp
+import net.nurigo.sdk.message.model.KakaoOption
 import net.nurigo.sdk.message.model.Message
 import org.aing.danurirest.global.exception.CustomException
 import org.aing.danurirest.global.exception.enums.CustomErrorCode
@@ -8,13 +9,15 @@ import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 
 @Service
-class SendSmsUsecase(
+class SendKakaoUsecase(
     @Value("\${sms.apiKey}")
     private val solApiKey: String,
     @Value("\${sms.apiSecret}")
     private val solApiSecretKey: String,
     @Value("\${sms.fromNumber}")
     private val solFrom: String,
+    @Value("\${sms.pfId}")
+    private val solPfId: String,
 ) {
     private val messageService =
         NurigoApp.initialize(
@@ -25,13 +28,22 @@ class SendSmsUsecase(
 
     fun execute(
         phone: String,
-        text: String,
+        template: String,
+        info: HashMap<String, String>,
     ) {
+        val kakaoOption = KakaoOption()
+
+        kakaoOption.apply {
+            pfId = solPfId
+            templateId = template
+            variables = info
+        }
+
         val message =
             Message(
                 from = solFrom,
                 to = phone.replace("-", ""),
-                text = text,
+                kakaoOptions = kakaoOption,
             )
 
         try {
