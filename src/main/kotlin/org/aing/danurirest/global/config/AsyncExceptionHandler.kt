f22224e55b44701d3c789d8f7c4669b4cd448ae9
Jihyuk Lee <withJihyuk@users.noreply.github.com>
2025-08-06 18:28:24 +0900

    feat :: 비동기 활용을 위한 관련 설정 구현

---
+++ src/main/kotlin/org/aing/danurirest/global/config/AsyncExceptionHandler.kt
@@ -0,0 +1,17 @@
+package org.aing.danurirest.global.config
+
+import org.slf4j.LoggerFactory
+import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler
+import java.lang.reflect.Method
+
+class AsyncExceptionHandler : AsyncUncaughtExceptionHandler {
+    private val log = LoggerFactory.getLogger(javaClass)
+
+    override fun handleUncaughtException(
+        e: Throwable,
+        method: Method,
+        vararg params: Any?,
+    ) {
+        log.error("Async Pool 에서 예외 발생. 메서드: {}, 파라미터: {}", method.name, params, e)
+    }
+}
