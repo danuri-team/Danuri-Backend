cf73ab29e2e044a3335e34209bf88b1f39fdac33
Jihyuk Lee <withJihyuk@users.noreply.github.com>
2025-08-16 17:01:07 +0900

    fix :: 잘못된 로거 가져오는 방법 수정

--- src/main/kotlin/org/aing/danurirest/global/util/GenerateQrCode.kt
+++ src/main/kotlin/org/aing/danurirest/global/util/GenerateQrCode.kt
@@ -3,12 +3,13 @@ package org.aing.danurirest.global.util
 import com.google.zxing.BarcodeFormat
 import com.google.zxing.MultiFormatWriter
 import com.google.zxing.client.j2se.MatrixToImageWriter
-import org.hibernate.query.sqm.tree.SqmNode.log
+import org.slf4j.LoggerFactory
 import java.io.ByteArrayOutputStream
 
 object GenerateQrCode {
     private const val WIDTH = 200
     private const val HEIGHT = 200
+    private val log = LoggerFactory.getLogger(this.javaClass)
 
     fun execute(usageId: String): Result<ByteArray> =
         try {
