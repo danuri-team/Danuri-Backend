20f4de9b21ff292f41d6a0fa22589b1187d9ee2e
Jihyuk Lee <withJihyuk@users.noreply.github.com>
2025-08-27 20:18:44 +0900

    feat :: `readOnly` 설정을 통한 성능 개선

--- src/main/kotlin/org/aing/danurirest/domain/form/usecase/GetSignUpFormUsecase.kt
+++ src/main/kotlin/org/aing/danurirest/domain/form/usecase/GetSignUpFormUsecase.kt
@@ -3,16 +3,18 @@ package org.aing.danurirest.domain.form.usecase
 import org.aing.danurirest.domain.form.dto.GetSignUpFormResponse
 import org.aing.danurirest.global.exception.CustomException
 import org.aing.danurirest.global.exception.enums.CustomErrorCode
-import org.aing.danurirest.global.security.util.PrincipalUtil
+import org.aing.danurirest.global.util.PrincipalUtil
 import org.aing.danurirest.persistence.device.repository.DeviceJpaRepository
 import org.aing.danurirest.persistence.form.repository.FormJpaRepository
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 @Service
 class GetSignUpFormUsecase(
     private val formJpaRepository: FormJpaRepository,
     private val deviceJpaRepository: DeviceJpaRepository,
 ) {
+    @Transactional(readOnly = true)
     fun execute(): GetSignUpFormResponse {
         val deviceId = PrincipalUtil.getUserId()
         val device = deviceJpaRepository.findById(deviceId).orElseThrow { CustomException(CustomErrorCode.NOT_FOUND_DEVICE) }
