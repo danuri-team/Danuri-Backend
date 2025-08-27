23c9a7f7cccf311ccfd62ec5e120e4bb80819f9c
Jihyuk Lee <withJihyuk@users.noreply.github.com>
2025-08-27 20:20:09 +0900

    delete :: `string` 타입에 맞는 제약 조건만 남기고 제거

--- src/main/kotlin/org/aing/danurirest/domain/admin/dto/UpdateDeviceRequest.kt
+++ src/main/kotlin/org/aing/danurirest/domain/admin/dto/UpdateDeviceRequest.kt
@@ -1,10 +1,8 @@
 package org.aing.danurirest.domain.admin.dto
 
 import jakarta.validation.constraints.NotBlank
-import jakarta.validation.constraints.NotNull
 
 data class UpdateDeviceRequest(
-    @field:NotNull(message = "공간 ID는 필수 입력값입니다.")
     @field:NotBlank(message = "별명은 공백일 수 없습니다.")
     val name: String,
-) 
+)
