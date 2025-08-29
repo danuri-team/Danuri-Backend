ee2b8745c393e008374cc21d578f40ae67f169c9
Jihyuk Lee <withJihyuk@users.noreply.github.com>
2025-08-29 10:11:38 +0900

    feat :: 존재하지 않는 엔드포인트 / 토큰 미 지정에 대한 예외 개선

---
+++ src/main/kotlin/org/aing/danurirest/global/security/handler/CustomAuthenticationEntryPoint.kt
@@ -0,0 +1,29 @@
+package org.aing.danurirest.global.security.handler
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+import org.aing.danurirest.global.exception.dto.CustomExceptionResponse
+import org.aing.danurirest.global.exception.enums.CustomErrorCode
+import org.springframework.http.MediaType
+import org.springframework.security.core.AuthenticationException
+import org.springframework.security.web.AuthenticationEntryPoint
+import org.springframework.stereotype.Component
+
+@Component
+class CustomAuthenticationEntryPoint(
+    private val objectMapper: ObjectMapper,
+) : AuthenticationEntryPoint {
+    override fun commence(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        authException: AuthenticationException,
+    ) {
+        val exceptionResponse = CustomExceptionResponse(CustomErrorCode.MISSING_TOKEN)
+        
+        response.status = exceptionResponse.status.status.value()
+        response.characterEncoding = Charsets.UTF_8.name()
+        response.contentType = MediaType.APPLICATION_JSON_VALUE
+        response.writer.print(objectMapper.writeValueAsString(exceptionResponse))
+    }
+}
