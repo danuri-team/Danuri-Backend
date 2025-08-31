33fa298c6086d6eca70732866826e5bf4de9584a
Jihyuk Lee <withJihyuk@users.noreply.github.com>
2025-08-31 11:58:17 +0900

    feat :: `401` 상태코드 리턴 시, 헤더 표준에 맞추기

--- src/main/kotlin/org/aing/danurirest/global/security/handler/CustomAuthenticationEntryPoint.kt
+++ src/main/kotlin/org/aing/danurirest/global/security/handler/CustomAuthenticationEntryPoint.kt
@@ -5,6 +5,7 @@ import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
 import org.aing.danurirest.global.exception.dto.CustomExceptionResponse
 import org.aing.danurirest.global.exception.enums.CustomErrorCode
+import org.springframework.http.HttpHeaders
 import org.springframework.http.MediaType
 import org.springframework.security.core.AuthenticationException
 import org.springframework.security.web.AuthenticationEntryPoint
@@ -20,10 +21,13 @@ class CustomAuthenticationEntryPoint(
         authException: AuthenticationException,
     ) {
         val exceptionResponse = CustomExceptionResponse(CustomErrorCode.MISSING_TOKEN)
-        
+
         response.status = exceptionResponse.status.status.value()
         response.characterEncoding = Charsets.UTF_8.name()
         response.contentType = MediaType.APPLICATION_JSON_VALUE
+        response.setHeader(HttpHeaders.WWW_AUTHENTICATE, "Bearer")
+        response.setHeader(HttpHeaders.CACHE_CONTROL, "no-store")
         response.writer.print(objectMapper.writeValueAsString(exceptionResponse))
+        response.writer.flush()
     }
 }
