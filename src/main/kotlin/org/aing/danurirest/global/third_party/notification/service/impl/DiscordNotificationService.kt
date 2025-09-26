62e55b0c8734a6314fa4e8589ef3d04676723fc1
Jihyuk Lee <withJihyuk@users.noreply.github.com>
2025-09-26 21:35:11 +0900

    feat :: 비동기 작업에 대한 로거 추가

--- src/main/kotlin/org/aing/danurirest/global/third_party/notification/service/impl/DiscordNotificationService.kt
+++ src/main/kotlin/org/aing/danurirest/global/third_party/notification/service/impl/DiscordNotificationService.kt
@@ -1,10 +1,14 @@
 package org.aing.danurirest.global.third_party.notification.service.impl
 
+import org.aing.danurirest.global.exception.CustomException
+import org.aing.danurirest.global.exception.enums.CustomErrorCode
 import org.aing.danurirest.global.third_party.notification.client.DiscordFeignClient
 import org.aing.danurirest.global.third_party.notification.dto.DiscordMessage
 import org.aing.danurirest.global.third_party.notification.service.NotificationService
 import org.aing.danurirest.global.third_party.notification.template.MessageTemplate
 import org.aing.danurirest.global.third_party.notification.template.MessageValueTemplate
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.context.annotation.Profile
 import org.springframework.stereotype.Service
 
@@ -13,6 +17,8 @@ import org.springframework.stereotype.Service
 class DiscordNotificationService(
     private val discordFeignClient: DiscordFeignClient,
 ) : NotificationService {
+    private val log: Logger = LoggerFactory.getLogger(DiscordNotificationService::class.java)
+
     override fun sendNotification(
         toMessage: String,
         template: MessageTemplate,
@@ -26,7 +32,11 @@ class DiscordNotificationService(
             ---
             $formattedMessage
             """.trimIndent()
-
-        discordFeignClient.sendMessage(DiscordMessage(content = discordMessageContent))
+        try {
+            discordFeignClient.sendMessage(DiscordMessage(content = discordMessageContent))
+        } catch (e: Exception) {
+            log.error("[비동기 작업 에러] 카카오톡 알림 발송에 실패했어요. ${e.message}")
+            throw CustomException(CustomErrorCode.UNKNOWN_SERVER_ERROR)
+        }
     }
 }
