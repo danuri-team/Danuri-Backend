2f966bf7e9d56eeb687a049d71c2ead3350b675c
Jihyuk Lee <withJihyuk@users.noreply.github.com>
2025-09-26 21:35:11 +0900

    feat :: 비동기 작업에 대한 로거 추가

--- src/main/kotlin/org/aing/danurirest/global/third_party/notification/service/impl/KakaoNotificationService.kt
+++ src/main/kotlin/org/aing/danurirest/global/third_party/notification/service/impl/KakaoNotificationService.kt
@@ -8,6 +8,8 @@ import org.aing.danurirest.global.exception.enums.CustomErrorCode
 import org.aing.danurirest.global.third_party.notification.service.NotificationService
 import org.aing.danurirest.global.third_party.notification.template.MessageTemplate
 import org.aing.danurirest.global.third_party.notification.template.MessageValueTemplate
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Profile
 import org.springframework.stereotype.Service
@@ -24,6 +26,8 @@ class KakaoNotificationService(
     @Value("\${sms.pfId}")
     private val solPfId: String,
 ) : NotificationService {
+    private val log: Logger = LoggerFactory.getLogger(KakaoNotificationService::class.java)
+
     private val messageService =
         NurigoApp.initialize(
             apiKey = solApiKey,
@@ -54,6 +58,7 @@ class KakaoNotificationService(
         try {
             messageService.send(message)
         } catch (e: Exception) {
+            log.error("[비동기 작업 에러] 카카오톡 알림 발송에 실패했어요. ${e.message}")
             throw CustomException(CustomErrorCode.UNKNOWN_SERVER_ERROR)
         }
     }
