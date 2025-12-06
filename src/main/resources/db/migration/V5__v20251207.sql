453741f9e467527e184b9ccd5a831c29c2543de0
Jihyuk Lee <withJihyuk@users.noreply.github.com>
2025-12-07 01:11:54 +0900

    fix :: `RENAME`으로 손쉽게 해결

--- src/main/resources/db/migration/V5__v20251207.sql
+++ src/main/resources/db/migration/V5__v20251207.sql
@@ -7,30 +7,13 @@ ALTER TABLE danuri_prod.usage_history
 ALTER TABLE danuri_prod.form_result
     DROP FOREIGN KEY FKfnsvjxihpgpncxskfcisqk61i;
 
-CREATE TABLE danuri_prod.users
-(
-    id         BINARY(16)  NOT NULL,
-    created_at datetime    NOT NULL,
-    updated_at datetime    NOT NULL,
-    company_id BINARY(16)  NULL,
-    phone      VARCHAR(30) NOT NULL,
-    CONSTRAINT pk_users PRIMARY KEY (id)
-);
-
 ALTER TABLE danuri_prod.space
     ADD allow_multi_space_booking BIT(1) NULL;
 
 ALTER TABLE danuri_prod.space
     MODIFY allow_multi_space_booking BIT(1) NOT NULL;
 
-ALTER TABLE danuri_prod.form_result
-    ADD CONSTRAINT FK_FORMRESULT_ON_USER FOREIGN KEY (user_id) REFERENCES danuri_prod.users (id);
-
-ALTER TABLE danuri_prod.usage_history
-    ADD CONSTRAINT FK_USAGEHISTORY_ON_USER FOREIGN KEY (user_id) REFERENCES danuri_prod.users (id);
-
-ALTER TABLE danuri_prod.users
-    ADD CONSTRAINT FK_USERS_ON_COMPANY FOREIGN KEY (company_id) REFERENCES danuri_prod.company (id);
+RENAME TABLE "USER" TO users;
 
 DROP TABLE danuri_prod.user;
 
